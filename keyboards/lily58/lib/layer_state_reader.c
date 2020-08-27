8ef007046eccf72c514320bb65931fee31decc49
Karl <vnietzchev@gmail.com>
2020-08-26 23:11:10 -0700

    lily58 layer names adjusted

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -4,10 +4,10 @@
 #include "lily58.h"
 
 #define L_BASE 0
-#define L_LOWER (1 << 1)
-#define L_RAISE (1 << 2)
-#define L_ADJUST (1 << 3)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
+#define L_SYMBOLS (1 << 1)
+#define L_NUMPAD (1 << 2)
+#define L_MEDIA (1 << 3)
+#define L_MEDIA_TRI (L_MEDIA | L_NUMPAD | L_SYMBOLS)
 
 char layer_state_str[24];
 
@@ -15,17 +15,17 @@ const char *read_layer_state(void) {
   switch (layer_state)
   {
   case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: DEFAULT");
     break;
-  case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+  case L_NUMPAD:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: NUMPAD");
     break;
-  case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
+  case L_SYMBOLS:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: SYMBOLS");
     break;
-  case L_ADJUST:
-  case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+  case L_MEDIA:
+  case L_MEDIA_TRI:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: MEDIA");
     break;
   default:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
